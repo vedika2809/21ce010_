73bbab2ff3d431668a5a783b08bac23dca74601f
21ce010 <110251453+21ce010@users.noreply.github.com>
2022-07-31 21:58:46 +0530

    Add files via upload

---
+++ q7.java
@@ -0,0 +1,26 @@
+//id-21ce010,name-vedikabhalodi
+import org.w3c.dom.ls.LSInput;
+
+public class q7 {
+    public static void main(String[] args) {
+        int n =7;
+        for(int i=0;i<7;i++)
+        {
+            for(int j=6-I;j>0;j--)
+            {
+                
+                System.out.print(" ");
+            }
+            
+            for(int k =0;k<=I;k++)
+            {
+                System.out.print((int)Math.pow(2,k)+" ");
+            }
+            for(int l =0;l<=i-1;l++)
+            {
+                System.out.print((int)Math.pow(2,(i-1)-l)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
